ddc0c628ddcd2f971d41fe729d4d1189e444fdc9
Pixeltica <pixeltica@gmail.com>
2026-03-07 12:47:20 +0000

    Add project files.

---
+++ ToolUpgradeBundles/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace ToolUpgradeBundles
+{
+    internal class ModEntry
+    {
+    }
+}
