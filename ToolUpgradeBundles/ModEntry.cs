23eb20c2ffb11e15284ed6f7a7904b8b49539a5c
Pixeltica <pixeltica@gmail.com>
2026-04-06 06:44:22 +0100

    Rewrote logic to only handle Trash Can upgrade, remove ClintUpgrade dependency.

--- ToolUpgradeBundles/ModEntry.cs
+++ ToolUpgradeBundles/ModEntry.cs
@@ -1,53 +1,30 @@
 ﻿using StardewModdingAPI;
-using StardewModdingAPI.Events;
 using StardewValley;
-using StardewValley.Tools;
-using StardewValley.Menus;
+using StardewValley.Triggers;
+using StardewValley.Delegates;
 
-namespace ToolUpgradeBundles
+public class ModEntry : Mod
 {
-    public class ModEntry : Mod
+    public override void Entry(IModHelper helper)
     {
-        public override void Entry(IModHelper helper)
-        {
-            helper.Events.Player.InventoryChanged += OnInventoryChanged;
-            helper.Events.Display.MenuChanged += this.OnMenuChanged;
-        }
-        // Reduce the 2 day upgrade time to 0 to give the player the tool immediately,
-        // play the "hold item over head" animation (condition to account for the trash can throwing a wobbly)
-        private void OnInventoryChanged(object sender, InventoryChangedEventArgs e)
-        {
-            if (Game1.player.daysLeftForToolUpgrade.Value > 0 && Game1.player.toolBeingUpgraded.Value != null)
-            {
-                Tool tool = Game1.player.toolBeingUpgraded.Value;
+        TriggerActionManager.RegisterAction("Pixeltica_UpgradeTrashCan", this.UpgradeTrashCan);
+    }
 
-                if (tool is GenericTool)
-                {
-                    tool.actionWhenClaimed();
-                }
-                else
-                {
-                    Game1.player.addItemToInventory(tool);
-                }
-                Game1.player.toolBeingUpgraded.Value = null;
-                Game1.player.daysLeftForToolUpgrade.Value = 0;
-                Game1.player.holdUpItemThenMessage(tool);
-                
-            }
-        }
-        // Close Clint's dialogue before it can open because I cannot figure out how to prevent the 
-        // "hold item over head" animation from interrupting the dialogue box T_T
-        private void OnMenuChanged(object sender, MenuChangedEventArgs e)
+    private bool UpgradeTrashCan(string[] args, TriggerActionContext context, out string error)
+    {
+        if (!ArgUtility.TryGet(args, 1, out string levelStr, out error, allowBlank: false))
         {
-            
-            if (e.NewMenu is StardewValley.Menus.DialogueBox dialogueBox)
-            {
-                string clintUpgradeText = Game1.content.LoadString("Strings\\StringsFromCSFiles:Tool.cs.14317");
-                if (dialogueBox.getCurrentString() == clintUpgradeText)
-                {
-                    Game1.activeClickableMenu = null;
-                }
-            }
+            return false;
         }
+
+        if (int.TryParse(levelStr, out int targetLevel))
+        {
+            Game1.player.trashCanLevel = targetLevel;
+            return true;
+        }
+
+        // In case I fob the wrong argument
+        error = $"Wrong upgrade level '{levelStr}'. Expected an integer (1=Copper, 2=Steel, 3=Gold, 4=Iridium).";
+        return false;
     }
 }
